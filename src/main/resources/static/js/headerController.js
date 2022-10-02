d768b83f7defa9a3c8de27936c4495203f202eee
irickdias <henriquedias180@gmail.com>
2022-10-01 21:09:13 -0300

    commit 3
    feito um pouco do frontend

---
+++ src/main/resources/static/js/headerController.js
@@ -0,0 +1,56 @@
+const header = {
+    data(){
+
+    },
+    methods: {
+
+    },
+    template:
+    `
+    <section>
+            <header id="headerMainContainer">        
+                <nav id="headerContainer">
+                    
+                    <div class="logoContainer">
+                        <a to="/"><img class="topLogo" src="./images/eleicoes2022.png" alt="Logo"/></a>
+                    </div>
+                    
+                        
+                    <div class="box-titulo">
+                        <h1 class="titulo">Eleições 2022</h1>
+                        
+                    </div>
+                    
+                    <!--<Link class="botao-voltar" to="/">Voltar</Link>
+                    <div class="loginContainer">
+                        <div class="login-userimg">
+                            <img src="" alt="Foto de usuário"/>
+                        </div>
+
+                        <div>
+                            <Link id="user" to={destiny} class="bold bt-login">Login</Link>
+                        </div>
+                        
+                    </div>-->
+                        
+                </nav>  
+
+                <nav id="Menu" class="bold">
+                    <ul id="menuItens">
+                        <li><a href="./index.html">Home</a></li>
+                        
+                        <li><a href="./partidos.html">Partidos</a></li>
+
+                        <li><a href="">Candidatos</a></li>
+
+                        <li><a href="">Cargos</a></li>
+                        
+                        <li><a href="">Eleições</a></li>
+                    </ul>
+                </nav>
+            </header>
+            
+        </section>
+    `
+}
+const vueheader = Vue.createApp(header).mount('#Header');
